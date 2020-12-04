e598db20be950a9c23a5df35bd11159126c0e685
Dmitry Druzhkov <d.druzhkoff@gmail.com>
2020-12-05 01:55:18 +0300

    Some tasks complete

--- ChatServer.cpp
+++ ChatServer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <uwebsockets/App.h>
 #include <regex>
+#include <map>
 
 using namespace std;
 
@@ -18,28 +19,45 @@ int main()
 {
     int port = 8888;
     unsigned long latest_user_id = 10;
+    unsigned long total_users = 0;
+    map<unsigned long, string> online_users;
 
     uWS::App().ws<UserConnection>("/*",
         {
-            .open = [&latest_user_id](auto* ws) {
+            .open = [&total_users, &latest_user_id, &online_users](auto* ws) {
                 UserConnection* data = (UserConnection*) ws->getUserData();
                 data->user_id = latest_user_id++;
                 data->name = "UNNAMED";
                 cout << "New user connected. ID = " << data->user_id << endl;
+                cout << "Total users: " << ++total_users << endl;
                 ws->subscribe("broadcast");
                 ws->subscribe("user#" + to_string(data->user_id));
                 //on connection open
+                online_users[data->user_id] = data->name;
             },          
-            .message = [](auto* ws, string_view message, uWS::OpCode opCode) {
+            .message = [&online_users](auto* ws, string_view message, uWS::OpCode opCode) {
                 string SET_NAME("SET_NAME=");
                 string MESSAGE_TO("MESSAGE_TO=");
 
                 UserConnection* data = (UserConnection*)ws->getUserData();
-                cout << "New messgae recieved: " << message << endl;
+                cout << "New message recieved: " << message << endl;
                 if (message.find(SET_NAME) == 0)
                 {
                     cout << "User sets their name" << endl;
-                    data->name = (string)message.substr(SET_NAME.length());
+                    string temp_name = (string)message.substr(SET_NAME.length());
+                    if (temp_name.find(',') != string::npos)
+                    {
+                        cout << "',' cannot be used in name" << endl;
+                        return;
+                    }
+                    if (temp_name.length() > 255)
+                    {
+                        cout << "It cannot be that big, can it?" << endl;
+                        return;
+                    }
+                    data->name = temp_name;
+                    ws->publish("broadcast", "NEW_USER=" + to_string(data->user_id) + "," + data->name);
+                    online_users[data->user_id] = data->name;
                 }
                 if (message.find(MESSAGE_TO) == 0)
                 {
@@ -47,7 +65,14 @@ int main()
                     int comma_position = rest.find(',');
                     auto ID = rest.substr(0, comma_position);
                     auto text = rest.substr(comma_position + 1);
-                    ws->publish("user#" + (string)ID, text);
+                    if (online_users.contains(std::stoi((string)ID)))
+                    {
+                        ws->publish("user#" + (string)ID, text);
+                    }
+                    else
+                    {
+                        ws->publish("user#" + to_string(data->user_id), "User not found");
+                    }
                 }
                 //on new message received
             }
